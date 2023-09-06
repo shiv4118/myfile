1c2f94b0b519f3346dc075a358112a4fd8b0e719
Shivam.P <Shivam.P@A4-G-408-WINDOWS.afourtech.pune>
2023-09-06 20:48:48 +0530

    newterraform

--- ec2-new.tf
+++ ec2-new.tf
@@ -1,24 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIARXKTQFUNNV36E4UU"
-  secret_key = "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"
-}
-
-resource "aws_instance" "test123" {
-    ami = "ami-06f621d90fa29f6d0"
-    instance_type = "t2.micro"
-
-    tags = {
-      name = "test12"
-    }
-  
 }
