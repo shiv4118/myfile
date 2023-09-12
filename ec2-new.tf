0bdfce4b0476695740a779c78b14b84f19eb55e8
Shivam.P <Shivam.P@A4-G-408-WINDOWS.afourtech.pune>
2023-09-12 20:01:01 +0530

    newterraform

--- ec2-new.tf
+++ ec2-new.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.37"
-    }
-  }
-}
 provider "aws" {
   region = "ap-south-1"
   access_key = "AKIARXKTQFUNNV36E4UU"
