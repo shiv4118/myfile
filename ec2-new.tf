5ac9680c63ec594a8c7a919fcea3be1545ec7752
Shivam.P <Shivam.P@A4-G-408-WINDOWS.afourtech.pune>
2023-09-12 20:19:41 +0530

    newterraform

--- ec2-new.tf
+++ ec2-new.tf
@@ -3,8 +3,3 @@ provider "aws" {
   access_key = "AKIARXKTQFUNNV36E4UU"
   secret_key = "Prgm9tlT72X1MPSiczDjKkZPxy1o0OvqS8UJwFdQ"
 }
-
-resource "aws_instance" "example" {
-  ami           = "ami-02bb7d8191b50f4bb"
-  instance_type = "t2.micro"
-}
